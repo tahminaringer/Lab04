b86bfdd86c2f12b44e0ecbac04dbe41d781c5d38
Tahmina Ringer <tahmina.ringer@gmail.com>
2020-08-20 16:24:14 -0700

    converted to function code

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 'use strict'
-
-var userName = prompt('Please Enter Your Name');
-
-if (userName === 'Tahmina') {
-    alert("Have A Glorious Day " + userName);
-} else {
-    alert("Have A Great Day " + userName);
+//Wrap this code in function
+function askUserForName(){
+    var userName = prompt('Please Enter Your Name ');
+    alert('Hello ' + userName);
+    return userName;
 }
-
-document.write('<h2>' + 'Welcome ' + userName)
-
-var question = prompt('What is your favorite coffee drink');
-
-document.write('<h2>' + 'yum ' + question + '</h2>')
-
-var emailQuestion = confirm("Would you like to join our mailing list?") 
-
-if (emailQuestion === true) {
     
-    var userPreference = prompt("What is your email address");
-}
+var userName = askUserForName();
 
-document.write('<h2>' + 'Great ' + userName + ' We have added ' + userPreference + ' to our mailing list ' + '<h2>')
+console.log(userName);
+
+function checkGreeting(){
+    if (userName === 'Tahmina'){
+        document.write('<h2>' + 'What a Glorious Day, welcome! ' + userName + '</h2>');
+    } else {
+        document.write('<h2>' + 'Welcome ' + userName + '</h2>');    
+    }
+}
+checkGreeting();
+
+function askFavoriteCoffeeDrink(){
+    var favoriteCoffeeDrink = prompt('What is your favorite coffee drink');
+    document.write('<h2>' + favoriteCoffeeDrink + ' yum! ' + '</h2>')
+    return favoriteCoffeeDrink;   
+}
+askFavoriteCoffeeDrink();
+
+function askJoinEmailList(){
+    var emailQuestion = confirm("Would you like to join our mailing list?"); 
+    return emailQuestion;
+}
+var emailQuestion = askJoinEmailList();
+
+function answer(emailQuestion){
+    if (emailQuestion === true){
+        var userPreference = prompt("What is your email address");
+        document.write('<h2>' + 'Great ' + userName + ' We have added ' + userPreference + ' to our mailing list ' + '<h2>')
+        return userPreference;
+    }
+}
+answer(emailQuestion);
